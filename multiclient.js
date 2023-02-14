9e8094c167cc3f969c634b440cef0a1b4085dfb2
Andrea Bernicchia <abernicchia@abernic-ltmrz50.internal.salesforce.com>
2023-02-14 13:05:00 +0100

    socket.io starting point

---
+++ multiclient.js
@@ -0,0 +1,56 @@
+const { io } = require("socket.io-client");
+
+const URL = process.env.CLIENT_WSSERVERURL || "http://localhost:8000";
+const MAX_CLIENTS = process.env.MAX_CLIENTS || 5;
+const CLIENT_CREATION_INTERVAL_IN_MS = process.env.CLIENT_CREATION_INTERVAL_IN_MS || 250;
+const EMIT_INTERVAL_IN_MS = process.env.EMIT_INTERVAL_IN_MS || 2000;
+
+let clientCount = 0;
+let lastReport = new Date().getTime();
+let packetsSinceLastReport = 0;
+
+console.info(`connecting to: ${URL}`);
+
+const createClient = () => {
+  // for demonstration purposes, some clients stay stuck in HTTP long-polling
+  const transports = ["websocket"];
+
+  const socket = io(URL, {
+    transports,
+  });
+
+  setInterval(() => {
+     socket.emit("c2s-event", Math.floor(Math.random() * 1000000));
+  }, EMIT_INTERVAL_IN_MS);
+
+  socket.on("seq-num", () => {
+    packetsSinceLastReport++;
+  });
+
+  socket.on("disconnect", (reason) => {
+    console.log(`disconnect due to ${reason}`);
+  });
+
+  if (++clientCount < MAX_CLIENTS) {
+    setTimeout(createClient, CLIENT_CREATION_INTERVAL_IN_MS);
+  }
+};
+
+createClient();
+
+const printReport = () => {
+  const now = new Date().getTime();
+  const durationSinceLastReport = (now - lastReport) / 1000;
+  const packetsPerSeconds = (
+    packetsSinceLastReport / durationSinceLastReport
+  ).toFixed(2);
+
+  console.log(
+    `client count: ${clientCount} ; average packets received per second: ${packetsPerSeconds}`
+  );
+
+  packetsSinceLastReport = 0;
+  lastReport = now;
+};
+
+setInterval(printReport, 5000);
